208c0dc4d64285b829a07b0cf1d69d5472ad31ea
userjia <Jharder@163.com>
2016-12-27 14:25:33 +0800

    core is ready

--- entity.h
+++ entity.h
@@ -7,11 +7,18 @@
 #define MASTER 0
 #define SLAVE 1
 
+struct timeStamp{
+    struct timeval tv;
+    struct timezone tz;
+};
+
 struct message{
     enum messageState messageType;
     unsigned short messageLength;
     unsigned short sequenceID;//报文序列号
     signed char logMessageInterval;//报文发送间隔
+    struct timeStamp stime;
+    struct timeStamp rtime;
 };
 
 enum deviceCharacter{
@@ -27,17 +34,21 @@ enum messageState{
 };
 
 enum deviceState{
-    wait_send,
     wait_recive,
+    wait_send,
     done,
 };
 
 struct device{
     int fd;
+    struct sockaddr_in addr;
+    struct sockaddr_in their_addr;
     enum deviceCharacter character;
     enum deviceState state;
     enum messageState msgState;
     struct message *msg[4];
+    struct timeval offset;
+    struct timeval delay;
 };
 
 #endif //TIMES_ENTITY_H
--- main.c
+++ main.c
@@ -8,38 +8,54 @@
 #include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 #include "entity.h"
 
 
 struct device dev;
+char *buf[sizeof(struct message)];
 
 long int out(enum messageState ms){
     struct message msg;
-    msg.messageType=ms;
-    long int len=send(dev.fd,&msg,sizeof(msg),0);
+    msg.messageType=ms;///msg is not full
+    gettimeofday(&msg.stime.tv,&msg.stime.tz);//send time
+
+    memset(buf,0,sizeof(buf));
+    memcpy(buf,&msg,sizeof(msg));
+    long int len=sendto(dev.fd,buf,sizeof(msg),0,(struct sockaddr *) &dev.addr, sizeof(dev.addr));// TODO struct point
+    if (len==-1) {
+        perror("send");
+    }
+
     return len;
 }
 
 struct message *receive(enum messageState ms){
     struct message *msg;
     msg=(struct message *)malloc(sizeof(struct message));
-    long int len=recv(dev.fd,msg,sizeof(*msg),0);
+    socklen_t addr_len=sizeof(dev.their_addr);
+
+    //char* buf[sizeof(msg)];
+    memset(buf,0,sizeof(buf));
+    long int len=recvfrom(dev.fd,buf,sizeof(buf),0,(struct sockaddr *) &dev.their_addr,&addr_len);
+    memset(msg,0,sizeof(msg));
+    memcpy(msg,buf,sizeof(buf));
+    gettimeofday(&msg->rtime.tv,&msg->rtime.tz);//recv time
     if (len!=-1) {
         return msg;
     }else{
+        perror("receive");
         return NULL;
     }
-
 }
 
-char *getTime(){
+/*char *getTime(){
     time_t t;
     struct tm *localTime;
     t= time(NULL);
     localTime=localtime(&t);
     return asctime(localTime);
-}
-
+}*/
 
 int getSocket(char *input_addr,char *port,char *socketType){
     int sockfd,fd;
@@ -52,35 +68,19 @@ int getSocket(char *input_addr,char *port,char *socketType){
     addr.sin_port=htons(atoi(port));
     addr.sin_addr.s_addr=inet_addr(input_addr);
 
-    if((sockfd=socket(PF_INET,SOCK_STREAM,0))==-1){
+    if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1){
         perror("create socket");
         return -1;
     }
-
+    dev.fd=sockfd;
     if (!strcmp(socketType,"server")){
-
         if (bind(sockfd,(struct sockaddr*)&addr,sizeof(struct sockaddr))==-1){
             perror("bind");
             return -1;
         }
 
-        if (listen(sockfd,lisnum)==-1){
-            perror("listen");
-            return -1;
-        }
-
-        if((fd=accept(sockfd,(struct sockaddr *)&their_addr,&socklen))==-1){
-            perror("accept");
-            return -1;
-        }
-
     }else if(!strcmp(socketType,"client")){
-
-          if (connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) != 0)
-        {
-            perror("Connect ");
-            exit(EXIT_FAILURE);
-        }
+        dev.addr=addr;
     }
 
     return fd;
@@ -95,53 +95,49 @@ int getSocket(char *input_addr,char *port,char *socketType){
 
 void syncTime(){
     while(1){
-        /*if (dev.character) {/////////////////segment fault?
-            dev.character=hostDecide();
-        }*/
-
-        /*switch(dev.msgState){
-            case sync:
-                if (dev.state==wait_send){
-                    out(sync);///////////////////can it just one sigle line to operate, like function array
-                }
-        }*/
         if (dev.state==wait_send) {
-            if(out(dev.msgState)!=-1){
-                if (dev.msgState==2&&dev.character==host) {
-                    dev.state=done;
-                    break;
-                }else if(dev.msgState<2){
-                    dev.msgState++;////////////////////////undone
-                    dev.state=wait_recive;
-                }else{
-                    break;
-                }
-
-            }else{
-                perror("send");
-            }
-        }else if(dev.state==wait_recive&&dev.msgState!=done){
-            dev.msg[dev.msgState]=receive(dev.msgState);
-            if (dev.msg[dev.msgState]!=NULL){
-                if (dev.msgState==3&&dev.character==slave) {
-                    dev.state=done;
-                    break;
-                }else if(dev.msgState<3){
-                    dev.msgState++;////////////////////////undone
-                    dev.state=wait_send;
-                }else{
-                    break;
-                }
-            }else{
-                perror("receive");
+            if(!out(dev.msgState)){
+                continue;//error,try again
             }
         }else{
-            break;
+            dev.msg[dev.msgState]=receive(dev.msgState);
+            if(dev.msg[dev.msgState]==NULL){
+                continue;
+            }
         }
+        if (dev.msgState==delay_resp&&((dev.state+dev.character)==1)) {// state and character different value
+            dev.state=done;
+            break;
+        }else if(dev.msgState!=sync){
+            dev.state=(dev.state/-1)+1;/////0 to 1 or 1 to 0
+        }
+        dev.state++;
     }
+
 }
 
+struct timeval add(struct timeval a,struct timeval b){
+    return {a.tv_sec-b.tv_sec,a.tv_usec-b.tv_usec};
+}
 
+struct timeval sub(struct timeval a,struct timeval b){
+    return {a.tv_sec+b.tv_sec,a.tv_usec+b.tv_usec};
+}
+
+struct timeval devide(struct timeval a){
+    return {a.tv_sec/2,a.tv_usec/2};
+}
+
+void calculate(){
+
+    struct timeval t1=dev.msg[0]->stime.tv;
+    struct timeval t2=dev.msg[0]->rtime.tv;
+    struct timeval t3=dev.msg[2]->stime.tv;
+    struct timeval t4=dev.msg[2]->rtime.tv;
+
+    dev.delay=devide(add(sub(t2,t1),sub(t4,t3)));
+    dev.offset=devide(sub(add(t1,t2),add(t4,t3)));
+}
 
 int main(int argc,char *argv[]){
     if(!strcmp(argv[1],"--help")){
@@ -157,9 +153,11 @@ int main(int argc,char *argv[]){
 
         if (strcasecmp(argv[3],"server")) {
             dev.character=host;//////////tempral host decide, server is host
+            dev.state=wait_send;
             dev.fd=getSocket(argv[1],argv[2],argv[3]);
         }else{
             dev.character=slave;
+            dev.state=wait_recive;
             dev.fd=getSocket(argv[1],argv[2],"client");
         }
 
@@ -175,6 +173,7 @@ int main(int argc,char *argv[]){
 
     while(1){
         syncTime();
+        calculate();
     }
 
 
